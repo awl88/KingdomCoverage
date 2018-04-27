94d1f30a8f7e53d0d94149bc69e2eb87065e210e
AndrewD <adufresplatinum@gmail.com>
2018-04-27 08:55:42 -0600

    messageDAO and impl

--- JPAKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/MessageDAO.java
+++ JPAKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/MessageDAO.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.jpakingdomcoverage.data;
 
+import com.skilldistillery.kingdomcoverage.entities.Message;
+
 public interface MessageDAO {
+
+	Message create(Message message);
+
+	boolean destroy(int id);
 
 }
--- JPAKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/MessageDAOImpl.java
+++ JPAKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/MessageDAOImpl.java
@@ -1,5 +1,44 @@
 package com.skilldistillery.jpakingdomcoverage.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.skilldistillery.kingdomcoverage.entities.Message;
+
+@Transactional
+@Component
 public class MessageDAOImpl implements MessageDAO {
 	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public Message create(Message message) {
+		// start the transaction
+		em.getTransaction().begin();
+		// write the insured to the database
+		em.persist(message);
+		// update the "local" Insured object
+		em.flush();
+		// commit the changes (actually perform the operation)
+		em.getTransaction().commit();
+		// return the Insured object
+		return message;
+	}
+	
+	@Override
+	public boolean destroy(int id) {
+		if (em.find(Message.class, id) == null) {
+			return false;
+		} else {
+			Message m = em.find(Message.class, id);
+			em.getTransaction().begin();
+			em.remove(m); // performs the delete on the managed entity
+			em.getTransaction().commit();
+			return true;
+		}
+	}
 }

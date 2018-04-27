e5d6be7f3647e6c09ca831f7288559c6e8e468bd
AndrewD <adufresplatinum@gmail.com>
2018-04-27 09:41:11 -0600

    test

--- JPAKingdomCoverage/src/main/java/com/skilldistillery/kingdomcoverage/entities/Agent.java
+++ JPAKingdomCoverage/src/main/java/com/skilldistillery/kingdomcoverage/entities/Agent.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
+// test comment
 @Entity
 public class Agent {
 	

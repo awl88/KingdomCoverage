218c3960b6c52c9fe71398d1382a904e218d2de1
Rebecca Allen <rebeccazarsky@gmail.com>
2018-04-27 09:59:50 -0600

    test commit

--- MVCKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/AddressDAOImpl.java
+++ MVCKingdomCoverage/src/main/java/com/skilldistillery/jpakingdomcoverage/data/AddressDAOImpl.java
@@ -3,6 +3,8 @@ package com.skilldistillery.jpakingdomcoverage.data;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.skilldistillery.kingdomcoverage.entities.Address;
 

6c46f5fddbf762cf7fee126917d2f4100dfd6a54
EliHarper <eli.harper1995@gmail.com>
2018-04-30 11:54:48 -0600

    Ready to start working on agent.jsp

--- MVCKingdomCoverage/src/main/java/com/skilldistillery/mvckingdomcoverage/controllers/AgentController.java
+++ MVCKingdomCoverage/src/main/java/com/skilldistillery/mvckingdomcoverage/controllers/AgentController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,8 @@ import com.skilldistillery.kingdomcoverage.entities.Agent;
 import com.skilldistillery.kingdomcoverage.entities.Insured;
 import com.skilldistillery.mvckingdomcoverage.data.AgentDAO;
 
+@Transactional
+@Controller
 public class AgentController {
 	@Autowired
 	AgentDAO adao;
